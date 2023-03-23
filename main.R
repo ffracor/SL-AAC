ccf94d5fb74904ecdc02b3a38b0dda859c482253
Francesco Corrini <fc.franci12@gmail.com>
2023-03-23 09:00:51 +0100

    regressione ignorante

--- main.R
+++ main.R
@@ -1,6 +1,10 @@
-dataset = read.csv("GitHub/SL-AAC/dataset.csv")
+dataset = read.csv("dataset.csv")
 
 #equivalenti (nome completo e sigla)
 unique(dataset$club)
 unique(dataset$club_abb)
 
+unique(dataset$birth_country)
+
+model = lm(data = dataset, X2021.guaranteed.comp. ~ - name - club_abb -club - birth_city - birth_country - foot - position - X2022.guar..comp..)
+summary(model)
