8ef42ff9eaa0c8106bc426f0eafc60426b9232e6
your_username <a.arici4@studenti.unibg.it>
2023-04-07 17:29:56 +0200

    finally works?

--- sleep.R
+++ sleep.R
@@ -34,8 +34,9 @@ train <- sample(dim(x)[1],floor(dim(x)[1]*0.75),replace = FALSE);
 
 mdl <- lm(Sleep.efficiency ~ . , data = x[train,])
 summary(mdl)
+mdl$coefficients
 
-fitt_value <- predict(mdl, newx = x[-train,])
+fitt_value <- predict.lm(mdl, x[-train,])
 
 test_MSE = mean((y[-train] - fitt_value)^2)
 test_MSE
@@ -45,7 +46,7 @@ get_alpha <- function(data,index){
   
   temp_train <- sample(nrow(data), floor(nrow(data) * 0.75), replace = FALSE)
   temp_mdl <- lm(Sleep.efficiency ~ . , data = data, subset = temp_train)
-  temp_fitt_value <- predict(temp_mdl, newx = data[-temp_train,])
+  temp_fitt_value <- predict(temp_mdl, data[-temp_train,])
   temp_test_MSE = mean((data$Sleep.efficiency[-temp_train] - temp_fitt_value)^2)
   return (temp_test_MSE)
 }
@@ -63,10 +64,12 @@ mean(pippo)
 step.model <- stepAIC(mdl, direction = "both", 
                       trace = FALSE)
 summary(step.model)
-fitt_value <- predict(step.model, newx = x[-train,])
+fitt_value <- predict(step.model,  x[-train,])
 step_test_MSE = mean((y[-train] - fitt_value)^2)
 step_test_MSE
 step.model$coefficients
+
+
 
 # Define bootstrap function
 get_alpha_SW <- function(data,index){
@@ -78,13 +81,13 @@ get_alpha_SW <- function(data,index){
   step.model <- stepAIC(mdl, direction = "both", 
                         trace = FALSE)
   
-  temp_fitt_value <- predict(step.model, newx =data[-temp_train,])
+  temp_fitt_value <- predict(step.model, data[-temp_train,])
   step_test_MSE = mean((data$Sleep.efficiency[-temp_train] - temp_fitt_value)^2)
   return (step_test_MSE)
 }
 
 # Use boot() function to perform bootstrap simulations
-res <- boot(x,get_alpha_SW,R=100)
+res <- boot(x,get_alpha_SW,R=1000)
 summary(res)
 pippo <- res[["t"]]
 hist(pippo)
@@ -103,43 +106,49 @@ set.seed(42);
 X <- model.matrix(Sleep.efficiency ~ . , data = x)
 Y <- y
 
-cv_lasso <- cv.glmnet(X[train,],Y[train], alpha = 1, nfolds = 10, lambda = NULL);
+cv_lasso <- cv.glmnet(X[train, ],Y[train], alpha = 1, nfolds = 10, lambda = NULL);
 plot(cv_lasso)
 lasso_opt_lambda = cv_lasso$lambda.min;
 lasso_opt_lambda
 
-glm_model_lasso = glmnet(X[train,],Y[train],alpha = 1, lambda = lasso_opt_lambda)
+glm_model_lasso <- glmnet(X[train, ],Y[train],alpha = 1, lambda = lasso_opt_lambda)
 summary(glm_model_lasso)
 glm_model_lasso$beta
 
-lasso_fit <- predict(glm_model_lasso,s = lasso_opt_lambda, newx = X[-train,])
+lasso_fit <- predict(glm_model_lasso, newx = X[-train, ], s = lasso_opt_lambda)
 lasso_test_MSE = mean((Y[-train] - lasso_fit)^2)
 lasso_test_MSE
 glm_model_lasso$beta
+
 
 #Bootstrap su lasso
 get_alpha_L <- function(data,index){
   
   temp_train <- sample(nrow(data), floor(nrow(data) * 0.75), replace = FALSE)
-  temp_cv_lasso <- cv.glmnet(data[temp_train,],data$Sleep.efficiency[temp_train], alpha = 1, nfolds = 10, lambda = NULL);
+  
+  temp_cv_lasso <- cv.glmnet(data[temp_train,], Y[temp_train], alpha = 1, nfolds = 10, lambda = NULL);
   lasso_opt_lambda = temp_cv_lasso$lambda.min;
   
-  glm_model_lasso = glmnet(X = data[temp_train,], Y = data$Sleep.efficiency[temp_train],alpha = 1, lambda = lasso_opt_lambda)
+  glm_model_lasso = glmnet(data[temp_train,], Y[temp_train],alpha = 1, lambda = lasso_opt_lambda)
   
   
   temp_fitt_value <- predict(glm_model_lasso, s = lasso_opt_lambda, newx = data[-temp_train,])
-  lasso_test_MSE = mean((data$Sleep.efficiency[-temp_train] - temp_fitt_value)^2)
+  lasso_test_MSE = mean((Y[-temp_train] - temp_fitt_value)^2)
   
   return (lasso_test_MSE)
+  
+  #return (c(lasso_opt_lambda, lasso_test_MSE))
 }
 
 # Use boot() function to perform bootstrap simulations
-res <- boot(x,get_alpha_L,R=100)
+res <- boot(X,get_alpha_L,R=1000)
 summary(res)
-pippo <- res[["t"]]
-hist(pippo)
-mean(pippo)
 
+pippo <- res[["t"]]
+#hist(pippo[,2])
+hist(pippo)
+#mean(pippo[,2])
+mean(pippo)
 
 
 
@@ -155,11 +164,20 @@ glm_model_ridge = glmnet(X[train,],Y[train],alpha = 0, lambda = ridge_opt_lambda
 summary(glm_model_ridge)
 glm_model_ridge$beta
 
-ridge_fit <- predict(glm_model_ridge,s = ridge_opt_lambda ,newx = X[-train,])
+ridge_fit <- predict(glm_model_ridge, s = ridge_opt_lambda , newx = X[-train,])
 ridge_test_MSE = mean((y[-train] - ridge_fit)^2)
 ridge_test_MSE
 
-# 
+
+
+
+
+
+
+
+
+
+#parte 2
 
 x = read.csv("Sleep_Efficiency.csv")
 x$Smoking.status <- as.numeric(as.factor(x$Smoking.status))
